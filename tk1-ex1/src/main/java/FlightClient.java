9d128ac13148d9afd94de2beab3d13834f4d1eae
roshianna <44615056+Marz-R@users.noreply.github.com>
2021-11-24 19:42:54 +0100

    Update FlightClient.java

--- tk1-ex1/src/main/java/FlightClient.java
+++ tk1-ex1/src/main/java/FlightClient.java
@@ -1,91 +1,144 @@
-import java.util.List;
-import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import interfaces.IFlightClient;
 import interfaces.IFlightServer;
 import model.Flight;
 import gui.FlightDetailsGUI;
-import gui.FlightListGUI;
 
-public class FlightClient implements IFlightClient {
+public class FlightServer implements IFlightServer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private static Logger logger = Logger.getLogger(FlightServer.class.getName());
 
-	// ui
-	private FlightListGUI flightListGUI = new FlightListGUI();
+	private List<IFlightClient> loggedInClients = new ArrayList<IFlightClient>();
+	private List<Flight> flights = new ArrayList<Flight>();
 
-	// global state
-	private String clientName;
-	private IFlightServer flightServer;
+	protected FlightServer() throws RemoteException{
+		super();
+		
+		// initialize with some flights
+		Flight flight1 = new Flight();
+		List<Integer> counters = Arrays.asList(100, 101, 102, 103, 104, 105);
+		List<String> gates = Arrays.asList("E12", "E13");
+		flight1.setFlightNum("LH1234");
+		flight1.setFlightInfo("Lufthansa", "A380", LocalDate.of(2021, 11, 24), "FRA", "HKG");
+		flight1.setDeparture(LocalDateTime.of(2021, 11, 24, 05, 12), 1, gates, LocalDateTime.of(2021, 11, 24, 05, 17));
+		flight1.setCheckIn("3", counters, LocalDateTime.of(2021, 11, 24, 00, 00), LocalDateTime.of(2021, 11, 24, 04, 42));
+		
+		Flight flight2 = new Flight();
+		gates = Arrays.asList("A03", "A04");
+		flight2.setFlightNum("CX4321");
+		flight2.setFlightInfo("Cathy Pacific", "B747", LocalDate.of(2021, 11, 25), "LAX", "FRA");
+		flight2.setArrival(LocalDateTime.of(2021, 11, 25, 15, 43), 1, gates, LocalDateTime.of(2021, 11, 25, 15, 43));
+		flight2.setStatus("B");
 
-	public FlightClient(String clientName) throws RemoteException {
-		this.clientName = clientName;
+		flights.add(flight1);
+		flights.add(flight2);
 	}
 
 	@Override
-	public void receiveListOfFlights(List<Flight> flights) {
-		logger.log(Level.INFO, "List of flights received: " + flights.size());
-		flightListGUI.setListOfFlights(flights);
+	public void login(String clientName, IFlightClient client) throws RemoteException {
+		if (loggedInClients.contains(client)) {
+			logger.log(Level.INFO, clientName + "has already logged in.");
+		} else {
+			loggedInClients.add(client);  // if client haven't logged in, add to list
+			logger.log(Level.INFO, "New client logged in: " + clientName);
+			
+			client.receiveListOfFlights(this.flights);  // send client current list of flights
+		}		
 	}
 
 	@Override
-	public void receiveUpdatedFlight(Flight flight, char operation) {
-		logger.log(Level.INFO, "Flight updated: " + flight.getFlightNum().toString());
-		flightListGUI.setUpdatedFlight(flight, operation);
+	public void logout(String clientName, IFlightClient client) throws RemoteException {
+		if (loggedInClients.contains(client)) {
+			loggedInClients.remove(client);
+			logger.log(Level.INFO, "Client logged out: " + clientName);
+		} else {
+			logger.log(Level.INFO, clientName + "doesn't exist.");
+		}
 	}
 	
-	// called by GUI
-	// send updated flight to server (call create/update/delete Flight function in FlightServer depending on value of operation)
-	public void sendUpdatedFlight(Flight flight, char operation) throws RemoteException {
-		logger.log(Level.INFO, "sendUpdatedFlight");
-		switch(operation) {
-			case 'C':
-				flightServer.createFlight(clientName, this, flight);
-				break;
-			case 'U':
-				flightServer.updateFlight(clientName, this, flight);
-				break;
-			case 'D':
-				flightServer.deleteFlight(clientName, this, flight);
-				break;
+	@Override
+	public void createFlight(String clientName, IFlightClient client, Flight flight) throws RemoteException{
+		if (loggedInClients.contains(client)) {
+			flights.add(flight);
+			
+			logger.log(Level.INFO, "Created flight: " + flight.getFlightNum().toString() + "by " + clientName);
+			informAllClients(flight, 'C');
+		} else {
+			logger.log(Level.INFO, "Invalid client: " + clientName);
 		}
 	}
 
-	public void logout() throws RemoteException {
-		flightServer.logout(clientName, this);
-	}
-	
-	public void startup() throws RemoteException, NotBoundException {
-		logger.log(Level.INFO, "Client start up");
-		// get the servers stub
-		Registry registry = LocateRegistry.getRegistry();
-		IFlightServer stubServer = (IFlightServer) registry.lookup("FlightServer");
-
-		// client creates a stub of itself to send it to the server
-		IFlightClient stubClient = (IFlightClient) UnicastRemoteObject.exportObject(this, 0);
-
-		this.flightServer = stubServer;
+	@Override
+	public void updateFlight(String clientName, IFlightClient client, Flight flight) throws RemoteException{
+		if (loggedInClients.contains(client)) {
+			for (Flight f : flights) {
+				if (f.getFlightNum() == flight.getFlightNum()) {  // assume flight number can function as the primary key
+					flights.set(flights.indexOf(f), flight);  // replace (i.e. update) existing flight data that have the same flight number with the received flight data
+					break;
+				}
+			}
 			
-		// calls the remote login method
-		this.flightListGUI = new FlightListGUI();
-		flightListGUI.setFlightClient(this);
-		flightListGUI.frmTkAirportArrivals.setVisible(true);  // start (open) FlightListGUI
-		
-		// calls the remote login method
-		stubServer.login(this.clientName, stubClient);
+			logger.log(Level.INFO, "Updated flight: " + flight.getFlightNum().toString() + "by " + clientName);
+			informAllClients(flight, 'U');
+		} else {
+			logger.log(Level.INFO, "Invalid client: " + clientName);
+		}
 	}
 
-	public static void main(String[] args) throws RemoteException, NotBoundException {
-		FlightClient client = new FlightClient(UUID.randomUUID().toString());
-		client.startup();
+	@Override
+	public void deleteFlight(String clientName, IFlightClient client, Flight flight) throws RemoteException{
+		if (loggedInClients.contains(client)) {
+			flights.remove(flights.indexOf(flight));
+			
+			logger.log(Level.INFO, "Deleted flight: " + flight.getFlightNum().toString() + "by " + clientName);
+			informAllClients(flight, 'D');
+		} else {
+			logger.log(Level.INFO, "Invalid client: " + clientName);
+		}	
+	}
+
+	// operation:
+	// C: create
+	// U: update
+	// D: delete
+	private void informAllClients(Flight flight, char operation) throws RemoteException{
+		for (IFlightClient client : loggedInClients) {
+			client.receiveUpdatedFlight(flight, operation);
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			// generate local registry			
+			FlightServer flightServer = new FlightServer();
+			IFlightServer stub = (IFlightServer) UnicastRemoteObject.exportObject(flightServer, 0);
+
+			// Bind the remote object's stub in the registry
+			Registry registry = LocateRegistry.createRegistry(1099);
+			registry.bind("FlightServer", stub);
+
+			logger.info("Server is ready");
+		} catch (Exception ex) {
+			logger.log(Level.SEVERE, "Server exception", ex);
+			ex.printStackTrace();
+		}
 	}
 
 }
