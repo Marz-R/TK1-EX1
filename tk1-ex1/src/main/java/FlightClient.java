7e7c558f79e8646d7cac342bee2691c923a0af4a
roshianna <44615056+Marz-R@users.noreply.github.com>
2021-11-24 19:42:38 +0100

    Update FlightClient.java

--- tk1-ex1/src/main/java/FlightClient.java
+++ tk1-ex1/src/main/java/FlightClient.java
@@ -38,13 +38,14 @@ public class FlightClient implements IFlightClient {
 
 	@Override
 	public void receiveUpdatedFlight(Flight flight, char operation) {
-		logger.log(Level.INFO, "Flight updated: " + flight.toString());
+		logger.log(Level.INFO, "Flight updated: " + flight.getFlightNum().toString());
 		flightListGUI.setUpdatedFlight(flight, operation);
 	}
 	
 	// called by GUI
 	// send updated flight to server (call create/update/delete Flight function in FlightServer depending on value of operation)
 	public void sendUpdatedFlight(Flight flight, char operation) throws RemoteException {
+		logger.log(Level.INFO, "sendUpdatedFlight");
 		switch(operation) {
 			case 'C':
 				flightServer.createFlight(clientName, this, flight);
@@ -65,8 +66,6 @@ public class FlightClient implements IFlightClient {
 	public void startup() throws RemoteException, NotBoundException {
 		logger.log(Level.INFO, "Client start up");
 		// get the servers stub
-		//Registry registry = LocateRegistry.getRegistry(Registry.REGISTRY_PORT);
-		//IFlightServer flightServer = (IFlightServer) registry.lookup("FlightServer");
 		Registry registry = LocateRegistry.getRegistry();
 		IFlightServer stubServer = (IFlightServer) registry.lookup("FlightServer");
 
@@ -75,9 +74,7 @@ public class FlightClient implements IFlightClient {
 
 		this.flightServer = stubServer;
 			
-		//stubServer.login(this.clientName, stubClient);
-		//flightServer.login(clientName, this);
-		
+		// calls the remote login method
 		this.flightListGUI = new FlightListGUI();
 		flightListGUI.setFlightClient(this);
 		flightListGUI.frmTkAirportArrivals.setVisible(true);  // start (open) FlightListGUI
